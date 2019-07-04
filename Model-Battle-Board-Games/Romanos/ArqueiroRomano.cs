a15f59a0d3b0edb143e326e84fb6d96143434e65
KetlenCamille <ketlen.rieper@gmail.com>
2019-07-04 20:59:56 -0300

    Criando nação Romana

--- Model-Battle-Board-Games/Romanos/ArqueiroRomano.cs
+++ Model-Battle-Board-Games/Romanos/ArqueiroRomano.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace BattleBoardGame.Model
 {
-    class ArqueiroEgipicio : Arqueiro
+    class ArqueiroRomano : Arqueiro
     {
 
     }
