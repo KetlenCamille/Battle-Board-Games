763319196bbad94bca6bd7eba74d1325a5a379ba
KetlenCamille <ketlen.rieper@gmail.com>
2019-07-04 21:04:20 -0300

    Criando FactoryExecitoRomano

---
+++ Model-Battle-Board-Games/Factory/FactoryExercitoRomano.cs
@@ -0,0 +1,26 @@
+﻿using BattleBoardGame.Model;
+using BattleBoardGame.Model.Factory;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelBattleBoardGames.Factory
+{
+    class FactoryExercitoRomano : AbstractFactoryExercito
+    {
+        public override Arqueiro CriarArqueiro()
+        {
+            return new ArqueiroRomano();
+        }
+
+        public override Cavaleiro CriarCavalaria()
+        {
+            return new CavaleiroRomano();
+        }
+
+        public override Guerreiro CriarGuerreiro()
+        {
+            return new GuerreiroRomano();
+        }
+    }
+}
