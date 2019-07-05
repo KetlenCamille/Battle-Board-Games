5964019f6ae2a8f6053ade520fb831a930b95a11
KetlenCamille <ketlen.rieper@gmail.com>
2019-07-04 21:02:17 -0300

    Criando FactoryExercitoEgipcio

---
+++ Model-Battle-Board-Games/Factory/FactoryExercitoEgipcio.cs
@@ -0,0 +1,26 @@
+﻿using BattleBoardGame.Model;
+using BattleBoardGame.Model.Factory;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelBattleBoardGames.Factory
+{
+    class FactoryExercitoEgipcio : AbstractFactoryExercito
+    {
+        public override Arqueiro CriarArqueiro()
+        {
+            return new ArqueiroEgipicio();
+        }
+
+        public override Cavaleiro CriarCavalaria()
+        {
+            return new CavaleiroEgipicia();
+        }
+
+        public override Guerreiro CriarGuerreiro()
+        {
+            return new GuerreiroEgipicio();
+        }
+    }
+}
